4e09666b999bfde68b637dc9b2682d76f26edb1d
Dean Cheemsan <dcheesman@gmail.com>
2014-02-21 14:54:45 -0700

    created an auto zoom based on percentage of progress

--- static/js/apps/mapDraw.js
+++ static/js/apps/mapDraw.js
@@ -1,36 +1,56 @@
+// the current amount of miles traveled
 var progressMiles = +location.search.replace(/^\?/,"");
 
+// manually setting latitude, longitude
 var startPoint = [40.24,-111.66];
 var endPoint = [40.6700, -73.9400];
 
-var tripDistanceLatLong = Math.sqrt( Math.pow(startPoint[0] - endPoint[0], 2) + Math.pow(startPoint[1] - endPoint[1], 2));
-
+// manual entry of distance in miles
 var tripDistanceMiles = 1972;
 
-var degreeToMilesratio = tripDistanceLatLong/tripDistanceMiles;
+// figure out the percentage of miles. Max of 1
+var progressPercent = Math.min(progressMiles/tripDistanceMiles, 1);
 
-var progressPercent = (degreeToMilesratio * progressMiles)/tripDistanceLatLong;
-
+// calculate the latitude longitude point based on percentage miles travelled
 var progressPoint = [startPoint[0] + (progressPercent * (endPoint[0]-startPoint[0])), startPoint[1] + (progressPercent * (endPoint[1]-startPoint[1]))];
 
-var map = L.map('map').setView(startPoint, 9);
+// create map
+var map = L.map('map');
+
+// select stame map style
 var stamen = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
 
+// start point pin
 var marker = L.marker(startPoint).addTo(map);
 marker.bindPopup("<b>Hello world!</b><br>We're going to the New York! " + Math.round(tripDistanceMiles) + " miles");
 
+// end point pin
 var marker2 = L.marker(endPoint).addTo(map);
-marker2.bindPopup("Degrees: " + tripDistanceLatLong);
+marker2.bindPopup("Degrees: ");
 
+// progress pin
 var progressMarker = L.marker(progressPoint).addTo(map);
-progressMarker.bindPopup("Progress: " + progressPoint[0] + ", " + progressPoint[1]);
+progressMarker.bindPopup("You've made it " + progressMiles + " miles!").openPopup();
 
+// progress line
 var polyline = L.polyline([
   startPoint,
   progressPoint
 ]).addTo(map);
 
-map.fitBounds(polyline.getBounds());
+// set the map to show distance
+if(progressPercent < .15){
+  map.fitBounds([
+    startPoint,
+    progressPoint
+    ])
+} else {
+  map.fitBounds([
+    startPoint,
+    endPoint
+    ]);
+
+}
 
 function incrementProgress(){
   location.href= "/?" + (progressMiles + 20);
