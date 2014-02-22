b1487552957c63828d69dfe0216355bf693c82cf
Dean Cheemsan <dcheesman@gmail.com>
2014-02-22 10:10:25 -0700

    fixed marker and polyline redraw

--- static/js/apps/mapDraw.js
+++ static/js/apps/mapDraw.js
@@ -13,7 +13,9 @@ var milestones = [
 // the current amount of miles traveled
 // var progressMiles = +location.search.replace(/^\?/,"");
 
-var progressMiles = 400;
+var progressMiles = .5;
+var lastProgressMiles = 0;
+
 $.ajax({
   url : "/teams/",
   method : "GET",
@@ -29,17 +31,16 @@ $.ajax({
   }
 }).done( function(){
   buildMap();
-
 });
 
+// manually setting latitude, longitude
+var startPoint = [40.24,-111.66];
+var endPoint = [40.6700, -73.9400];
+
+// manual entry of distance in miles
+var tripDistanceMiles = 1972;
 
 function buildMap(){
-  // manually setting latitude, longitude
-  var startPoint = [40.24,-111.66];
-  var endPoint = [40.6700, -73.9400];
-
-  // manual entry of distance in miles
-  var tripDistanceMiles = 1972;
 
   // figure out the percentage of miles. Max of 1
   var progressPercent = Math.min(progressMiles/tripDistanceMiles, 1);
@@ -47,45 +48,22 @@ function buildMap(){
   // calculate the latitude longitude point based on percentage miles travelled
   var progressPoint = [startPoint[0] + (progressPercent * (endPoint[0]-startPoint[0])), startPoint[1] + (progressPercent * (endPoint[1]-startPoint[1]))];
 
-
-  // select stame map style
-  // var stamen = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
-  var stamen = L.tileLayer('http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
-  // var stamen = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
-  // var openStreet = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
-
-  // start point pin
-  var marker = L.marker(startPoint).addTo(map);
-  marker.bindPopup("<b>Hello world!</b><br>We're going to the New York! " + Math.round(tripDistanceMiles) + " miles");
-
-  // end point pin
-  var marker2 = L.marker(endPoint).addTo(map);
-  marker2.bindPopup((tripDistanceMiles - progressMiles) + " Miles To Go!");
-
   // progress pin
-  var progressMarker = L.marker(progressPoint).addTo(map);
+  // var progressMarker = L.marker(progressPoint).addTo(map);
+  progressMarker.setLatLng(progressPoint).update();
   progressMarker.bindPopup("You've made it " + progressMiles + " miles!").openPopup();
 
   // progress line
-  var polyline = L.polyline([
+  polyline.setLatLngs([
     startPoint,
     progressPoint
-  ]).addTo(map);
+  ]);
 
-  // drop milestone pins reached. and the next one to be reached
   milestones.forEach(function(k){
-    if(k.distance <progressMiles){
+    if(k.distance >= lastProgressMiles && k.distance < progressMiles){
       L.circleMarker(milesToPoint(k.distance), {riseOnHover : true}).setRadius(6).addTo(map).bindPopup(k.distance + "Miles! " + k.label);
     }
   });
-
-  // convert a miles number into a latitude, longitude point
-  function milesToPoint(miles){
-    var percent = Math.min(miles/tripDistanceMiles, 1);
-    var point = [startPoint[0] + (percent * (endPoint[0]-startPoint[0])), startPoint[1] + (percent * (endPoint[1]-startPoint[1]))];
-    return point;
-  }
-
 
   // zoom in if the progress is small. Zoom out if you're above a specified percent
   if(progressPercent < .50){
@@ -100,12 +78,50 @@ function buildMap(){
       ]);
 
   }
+  lastProgressMiles = progressMiles;
+}
+
+// convert a miles number into a latitude, longitude point
+function milesToPoint(miles){
+  var percent = Math.min(miles/tripDistanceMiles, 1);
+  var point = [startPoint[0] + (percent * (endPoint[0]-startPoint[0])), startPoint[1] + (percent * (endPoint[1]-startPoint[1]))];
+  return point;
 }
 
 // create map
 var map = L.map('map', {
   scrollWheelZoom: false
 });
+
+// select stame map style
+// var stamen = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
+var stamen = L.tileLayer('http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
+// var stamen = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {attribution: 'Add some attributes here!'}).addTo(map);
+// var openStreet = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+// progress pin
+var progressMarker = L.marker(startPoint).addTo(map);
+progressMarker.bindPopup("You've made it " + progressMiles + " miles!").openPopup();
+
+// start point pin
+var marker = L.marker(startPoint).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>We're going to the New York! " + Math.round(tripDistanceMiles) + " miles");
+
+// end point pin
+var marker2 = L.marker(endPoint).addTo(map);
+marker2.bindPopup((tripDistanceMiles - progressMiles) + " Miles To Go!");
+
+// drop milestone pins reached. and the next one to be reached
+milestones.forEach(function(k){
+  if(k.distance < progressMiles){
+    L.circleMarker(milesToPoint(k.distance), {riseOnHover : true}).setRadius(6).addTo(map).bindPopup(k.distance + "Miles! " + k.label);
+  }
+});
+
+var polyline = L.polyline([
+    startPoint,
+    startPoint
+  ]).addTo(map);
 
 
 function incrementProgress(){
